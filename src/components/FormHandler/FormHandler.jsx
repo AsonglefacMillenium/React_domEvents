36af7a8c57020deb1e681840225fe621b382cc16
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2022-05-19 11:18:03 +0100

    setting timeout for submission status tex

--- src/components/FormHandler/FormHandler.jsx
+++ src/components/FormHandler/FormHandler.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import './FormHandler.css'
+import React, { useState } from "react";
+import "./FormHandler.css";
 
 const FormHandler = () => {
+  const [submitting, setSubmitting] = useState(false);
 
-    //Handling form submit, prevent deafault and for onsubmit event
-    const handleSubmit = (e) =>{
-e.preventDefault();
-alert('Form submitted successfully')
-    }
+  //Handling form submit, prevent deafault and for onsubmit event
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //set submission status text true when button is clicked
+    setSubmitting(true);
+
+    //set timeout for submission text to disappear
+
+    setTimeout(() =>{
+        setSubmitting(false);
+    }, 5000);
+
+    // alert("Form submitted successfully");
+  };
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
+      <p>Buy some Mangoes</p>
 
-    <p>Buy some Mangoes</p>
+      {/*action to load before submission */}
 
-    <form action="" onSubmit={handleSubmit}>
+      {submitting && <div>submiiting form ...</div>}
+
+      <form action="" onSubmit={handleSubmit}>
         <fieldset>
-            <label htmlFor="">
-                <p>Name: </p>
-                <input type="text" name='name' placeholder='Type of Mango' />
-            </label>
+          <label htmlFor="">
+            <p>Name: </p>
+            <input type="text" name="name" placeholder="Type of Mango" />
+          </label>
         </fieldset>
 
-        <button type='submit'>Submit</button>
-    </form>
-
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default FormHandler
+export default FormHandler;
